a1896c5c004269eaaab36e5f4a65d610ca2e2b24
ErnieBurrows <114937798+ErnieBurrows@users.noreply.github.com>
2025-08-04 14:35:46 -0400

    Added all animations to the project

--- Source/BucketKnight/BucketKnightGameMode.cpp
+++ Source/BucketKnight/BucketKnightGameMode.cpp
@@ -7,7 +7,7 @@
 ABucketKnightGameMode::ABucketKnightGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_BucketKnight"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
